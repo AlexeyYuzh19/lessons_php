5a4e914befa8c978199a84b1fcdf3aba7f229ccb
AlexeyYuzh19 <9676331836@mail.ru>
2023-05-16 10:27:08 +0500

    добавил meta - теги в файле homework_2.php

--- homework_2.php
+++ homework_2.php
@@ -15,7 +15,10 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, maximum-scale=1">
+    <meta name="Author" content="Южанин Алексей">
+    <meta name="Copyright" content="(c) Iuzhanin Alexey, никакая часть данного документа не может быть воспроизведена
+                в какой-то бы ни было форме без разрешения владельца авторских прав.">
     <title><?= $title; ?></title>
     <style>
         header, footer {
