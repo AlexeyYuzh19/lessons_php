b1c3d4bdbfac0004a569d845da222334498db39a
AlexeyYuzh19 <9676331836@mail.ru>
2023-05-19 13:54:19 +0500

    актуализация ссылок в файле homework_3.php

--- homework_3.php
+++ homework_3.php
@@ -17,22 +17,22 @@ $translit = [
     'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'yo', 'ж' => 'zh', 'з' => 'z',
     'и' => 'i', 'й' => 'y', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o', 'п' => 'p', 'р' => 'r',
     'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'ts', 'ч' => 'ch', 'ш' => 'sh', 'щ' => 'sch',
-    'ъ' => '', 'ы' => 'y', 'ь' => '', 'э' => 'e', 'ю' => 'yu', 'я' => 'ya', 'А' => 'A', 'Б' => 'B', 'В' => 'V',
+    'ъ' => '/', 'ы' => 'y', 'ь' => '/', 'э' => 'e', 'ю' => 'yu', 'я' => 'ya', 'А' => 'A', 'Б' => 'B', 'В' => 'V',
     'Г' => 'G', 'Д' => 'D', 'Е' => 'E', 'Ё' => 'Yo', 'Ж' => 'Zh', 'З' => 'Z', 'И' => 'I', 'Й' => 'Y', 'К' => 'K',
     'Л' => 'L', 'М' => 'M', 'Н' => 'N', 'О' => 'O', 'П' => 'P', 'Р' => 'R', 'С' => 'S', 'Т' => 'T', 'У' => 'U',
-    'Ф' => 'F', 'Х' => 'H', 'Ц' => 'Ts', 'Ч' => 'Ch', 'Ш' => 'Sh', 'Щ' => 'Sch', 'Ъ' => '', 'Ы' => 'Y', 'Ь' => '',
+    'Ф' => 'F', 'Х' => 'H', 'Ц' => 'Ts', 'Ч' => 'Ch', 'Ш' => 'Sh', 'Щ' => 'Sch', 'Ъ' => '/', 'Ы' => 'Y', 'Ь' => '/',
     'Э' => 'E', 'Ю' => 'Yu', 'Я' => 'Ya', '.' => '.', ',' => ',', ':' => ':', ';' => ';', '-' => '-'
 ];    // объявляем массив с транслитерацией
 $menuItems = [
-    ['title' => 'Главная', 'link' => '/', 'subitems' => []],
-    ['title' => 'О нас', 'link' => '/about', 'subitems' => [
-        ['title' => 'История', 'link' => '/about/history'],
-        ['title' => 'Команда', 'link' => '/about/team'],
-        ['title' => 'Контакты', 'link' => '/about/contacts']
+    ['title' => 'Главная', 'link' => 'https://gb.ru/', 'subitems' => []],
+    ['title' => 'О нас', 'link' => 'https://gb.ru/company', 'subitems' => [
+        ['title' => 'Программы обучения', 'link' => 'https://gb.ru/courses/all'],
+        ['title' => 'Мероприятия', 'link' => 'https://gb.ru/events'],
+        ['title' => 'Обратная связь', 'link' => 'https://gb.ru/ask.sergey']
     ]],
-    ['title' => 'Услуги', 'link' => '/services', 'subitems' => [
-        ['title' => 'Веб-разработка', 'link' => '/services/web'],
-        ['title' => 'Мобильная разработка', 'link' => '/services/mobile']
+    ['title' => 'Поиск работы', 'link' => 'https://gb.ru/career', 'subitems' => [
+        ['title' => 'Тестирование', 'link' => 'https://gb.ru/tests'],
+        ['title' => 'Вакансии', 'link' => 'https://gb.ru/career']
     ]]
 ];
 ?>
@@ -264,7 +264,7 @@ $menuItems = [
         {
             $result = '<ul>';
             foreach ($menuItems as $item) {
-                $result .= '<li><a href="' . $item['link'] . '">' . $item['title'] . '</a>';
+                $result .= '<li><a href="' . $item['link'] . ' "target="_blank">' . $item['title'] . '</a>';
                 if (!empty($item['subitems'])) {
                     $result .= generateMenu($item['subitems']);
                 }
